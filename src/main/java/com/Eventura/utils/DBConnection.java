e6283e7a9b02289e545c7d815c83d7422c5e0f3e
Lukshan <lukshan@example.com>
2025-05-03 19:35:10 +0530

    lukshan commit packages

---
+++ src/main/java/com/Eventura/utils/DBConnection.java
@@ -0,0 +1,26 @@
+package com.Eventura.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+		private static String url = "jdbc:mysql://localhost:3306/eventplan";
+		private static String user = "root";
+		private static String pass = "Lukshan";
+		private static Connection con;
+		
+		public static Connection getConnection(){
+			
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection(url, user, pass);
+			}
+			catch(Exception e) {
+				
+				System.out.println("Database Not Connect !");
+				e.printStackTrace();
+			}
+			return con;
+		}
+}
